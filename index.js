acec429e29c553a986b06b4c74d4007087b477f1
EdrickMuthamia <edrickmuthamia05@gmail.com>
2025-06-04 17:27:41 +0300

    Add initial implementation of variables for company details

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+// 1. Create a variable named companyName and set it to 'Scuber' using const
+const companyName = 'Scuber';
+
+// 2. Create a variable named mostProfitableNeighborhood and set it to any string (use let)
+let mostProfitableNeighborhood = 'Chelsea';
+
+// 3. Create a variable named companyCeo and set it to any string (use let)
+let companyCeo = 'Susan Smith';
